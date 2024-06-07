c4909d39b15fdf60514034f9803f13e3969a53c3
voidProgramms <isip_r.o.biktimirov@mpt.ru>
2024-06-07 23:23:20 +0300

    Добавление тем, папки под элементы и скрипта БД

--- UMIASApp/App.xaml.cs
+++ UMIASApp/App.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using UMIASApp.Properties;
 
 namespace UMIASApp
 {
@@ -13,5 +9,31 @@ namespace UMIASApp
     /// </summary>
     public partial class App : Application
     {
+
+        private static string _theme;
+
+        public App()
+        {
+            InitializeComponent();
+            Theme = Settings.Default.CurrentTheme;
+        }
+
+        public static string Theme
+        {
+            get => _theme;
+            set
+            {
+                _theme = value;
+                var dick = new ResourceDictionary
+                { Source = new Uri($"pack://application:,,,/Themes;component/{value}.xaml", UriKind.Absolute) };
+                Current.Resources.MergedDictionaries.RemoveAt(0);
+                Current.Resources.MergedDictionaries.Insert(0, dick);
+
+                Settings.Default.CurrentTheme = value;
+                Settings.Default.Save();
+            }
+        }
+
+
     }
 }
