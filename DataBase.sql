1cba43753c0043a19d3296e44d70b696915cfc92
Sapgor <vasyuckov.n@yandex.ru>
2024-06-14 18:26:15 +0300

    Заполнение бд

--- DataBase.sql
+++ DataBase.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE Pract10;
+﻿CREATE DATABASE UMIAS;
 GO
 
-USE Pract10;
+USE UMIAS;
 GO
 
-CREATE TABLE Admin (
+CREATE TABLE Admins (
     IdAdmin int IDENTITY,
-    Surname varchar(50) NOT NULL,
-    Name varchar(50) NOT NULL,
+    Surname nvarchar(50) NOT NULL,
+    Admin_Name nvarchar(50) NOT NULL,
     Patronymic nvarchar(50) NULL,
-    EnterPassword varchar(50) NOT NULL,
+    EnterPassword nvarchar(50) NOT NULL,
     PRIMARY KEY (IdAdmin)
 );
 GO
 
-INSERT INTO Admin (Surname, Name, Patronymic, EnterPassword) 
+INSERT INTO Admins (Surname, Admin_Name, Patronymic, EnterPassword) 
 VALUES 
 ('Biktimirov', 'Ruslan', 'Ruslanovich', 'Ruslan123'),
 ('Sinitsyna', 'Lisa', 'Lisovna', 'Lisa123'),
@@ -25,12 +25,12 @@ GO
 
 CREATE TABLE Specialities (
     IdSpeciality int IDENTITY,
-    Name varchar(50) NOT NULL,
+    Speciality_Name varchar(50) NOT NULL,
     PRIMARY KEY (IdSpeciality)
 );
 GO
 
-INSERT INTO Specialities (Name) 
+INSERT INTO Specialities (Speciality_Name) 
 VALUES 
 ('Therapist'), 
 ('Surgeon'), 
@@ -39,33 +39,33 @@ GO
 
 CREATE TABLE Patient (
     OMS bigint NOT NULL,
-    Surname varchar(50) NOT NULL,
-    Name varchar(50) NOT NULL,
+    Surname nvarchar(50) NOT NULL,
+    Patient_Name nvarchar(50) NOT NULL,
     Patronymic nvarchar(50) NULL,
     BirthDate date NOT NULL,
-    Address varchar(255) NOT NULL,
-    LivingAddress varchar(255) NULL,
+    Patient_Address nvarchar(255) NOT NULL,
+    LivingAddress nvarchar(255) NULL,
     Phone nvarchar(18) NULL,
     Email nvarchar(50) NULL,
-    Nickname varchar(50) NULL,
+    Nickname nvarchar(50) NULL,
     PRIMARY KEY (OMS)
 );
 GO
 
-INSERT INTO Patient (OMS, Surname, Name, Patronymic, BirthDate, Address, LivingAddress, Phone, Email, Nickname) 
+INSERT INTO Patient (OMS, Surname, Patient_Name, Patronymic, BirthDate, Patient_Address, LivingAddress, Phone, Email, Nickname) 
 VALUES 
 (1234567890417542, 'Sidorov', 'Stepan', 'Petrovich', '1990-01-15', 'st. Lenina, h. 1', 'st. Nezhinskaya, h.20', '+7 (999) 123-45-67', 'example@mail.com', 'ivan89'),
 (2345678901334785, 'Petrova', 'Maria', 'Igorevna', '1985-07-20', 'st. Stalina, h. 5', 'st. Middle h.10', '+7 (999) 456-78-90', 'masha85@mail.com', 'Maria52');
 GO
 
-CREATE TABLE Status (
+CREATE TABLE Statuses (
     IdStatus int IDENTITY,
-    Name varchar(50) NOT NULL,
+    Status_Name nvarchar(50) NOT NULL,
     PRIMARY KEY (IdStatus)
 );
 GO
 
-INSERT INTO Status (Name) 
+INSERT INTO Statuses (Status_Name) 
 VALUES 
 ('Active'), 
 ('On treatment'), 
@@ -73,10 +73,10 @@ VALUES
 GO
 
 CREATE TABLE Directions (
-    Iddirection char(18) NOT NULL,
+    IdDirection char(18) NOT NULL,
     IdSpeciality int NOT NULL,
     OMS bigint NOT NULL,
-    PRIMARY KEY (Iddirection),
+    PRIMARY KEY (IdDirection),
     FOREIGN KEY (IdSpeciality) REFERENCES Specialities(IdSpeciality),
     FOREIGN KEY (OMS) REFERENCES Patient(OMS)
 );
@@ -90,25 +90,25 @@ GO
 
 CREATE TABLE Doctor (
     IdDoctor int IDENTITY,
-    Surname varchar(50) NOT NULL,
-    Name varchar(50) NOT NULL,
+    Surname nvarchar(50) NOT NULL,
+    Doctor_Name nvarchar(50) NOT NULL,
     Patronymic nvarchar(50) NULL,
     IdSpeciality int NOT NULL,
-    EnterPassword varchar(50) NOT NULL,
-    WorkAddress varchar(50) NOT NULL,
+    EnterPassword nvarchar(50) NOT NULL,
+    WorkAddress nvarchar(50) NOT NULL,
     PRIMARY KEY (IdDoctor),
     FOREIGN KEY (IdSpeciality) REFERENCES Specialities(IdSpeciality)
 );
 GO
 
-INSERT INTO Doctor (Surname, Name, Patronymic, IdSpeciality, EnterPassword, WorkAddress) 
+INSERT INTO Doctor (Surname, Doctor_Name, Patronymic, IdSpeciality, EnterPassword, WorkAddress) 
 VALUES 
-('Petrov', 'Alexey', 'Igorevich', 1, 'doctor123', 'clinic �1'),
+('Petrov', 'Alexey', 'Igorevich', 1, 'doctor123', 'clinic ¹1'),
 ('Ivanova', 'Elena', 'Evgenevna', 3, 'doc456', 'children clinic');
 GO
 
 CREATE TABLE Appointments (
-    Idappointment int IDENTITY,
+    IdAppointment int IDENTITY,
     OMS bigint NULL,
     IdDoctor int NOT NULL,
     AppointmentDate date NOT NULL,
@@ -117,7 +117,7 @@ CREATE TABLE Appointments (
     PRIMARY KEY (Idappointment),
     FOREIGN KEY (OMS) REFERENCES Patient(OMS),
     FOREIGN KEY (IdDoctor) REFERENCES Doctor(IdDoctor),
-    FOREIGN KEY (IdStatus) REFERENCES Status(IdStatus)
+    FOREIGN KEY (IdStatus) REFERENCES Statuses(IdStatus)
 );
 GO
 
@@ -128,42 +128,42 @@ VALUES
 GO
 
 CREATE TABLE AppointmentDocument (
-    Midappointment int NOT NULL,
+    IdAppointment int NOT NULL,
     Rtf nvarchar(max) NOT NULL,
-    PRIMARY KEY (Midappointment),
-    FOREIGN KEY (Midappointment) REFERENCES Appointments(Idappointment)
+    PRIMARY KEY (IdAppointment),
+    FOREIGN KEY (IdAppointment) REFERENCES Appointments(Idappointment)
 );
 GO
 
-INSERT INTO AppointmentDocument (Midappointment, Rtf)
+INSERT INTO AppointmentDocument (IdAppointment, Rtf)
 VALUES 
 (1, 'Ticket1'),
 (2, 'Ticket2');
 GO
 
 CREATE TABLE AnalyseDocument (
-    Midappointment int NOT NULL,
+    IdAppointment int NOT NULL,
     Rtf nvarchar(max) NOT NULL,
-    PRIMARY KEY (Midappointment),
-    FOREIGN KEY (Midappointment) REFERENCES Appointments(Idappointment)
+    PRIMARY KEY (IdAppointment),
+    FOREIGN KEY (IdAppointment) REFERENCES Appointments(Idappointment)
 );
 GO
 
-INSERT INTO AnalyseDocument (Midappointment, Rtf)
+INSERT INTO AnalyseDocument (IdAppointment, Rtf)
 VALUES 
 (1, 'Result1'),
 (2, 'Result2');
 GO
 
 CREATE TABLE ResearchDocument (
-    Midappointment int NOT NULL,
+    IdAppointment int NOT NULL,
     Rtf nvarchar(max) NOT NULL,
-    PRIMARY KEY (Midappointment),
-    FOREIGN KEY (Midappointment) REFERENCES Appointments(Idappointment)
+    PRIMARY KEY (IdAppointment),
+    FOREIGN KEY (IdAppointment) REFERENCES Appointments(Idappointment)
 );
 GO
 
-INSERT INTO ResearchDocument (Midappointment, Rtf)
+INSERT INTO ResearchDocument (IdAppointment, Rtf)
 VALUES 
 (1, 'Documnet1'),
 (2, 'Document2');
