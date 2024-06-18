c935feb71f826d77054c9a259a6c6db2df46e035
GISPro <gleboss2517@yandex.ru>
2024-06-18 23:07:00 +0300

    NewCommit1

---
+++ UMIASWPF/Model/DoctorModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace UMIASWPF.Model
+{
+    public class DoctorModel
+    {
+        public int? IdDoctor { get; set; }
+
+        public string Surname { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string Patronymic { get; set; } = null!;
+
+        public int? SpecialityId { get; set; }
+
+        public string EnterPassword { get; set; } = null!;
+
+        public string WorkAddress { get; set; } = null!;
+    }
+}
---
+++ UMIASWPF/View/User/PatientWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UMIASWPF.View.User.Pages;
+using UMIASWPF.ViewModel.PatientViewModels;
+
+namespace UMIASWPF.View.User
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWindow.xaml
+    /// </summary>
+    public partial class PatientWindow : Window
+    {
+
+        public PatientWindow()
+        {
+            InitializeComponent();
+            DataContext = new PatientViewModel();
+            Frame.Content = new MainPage();
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Close();
+        }
+
+        private void UnwrapButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void RollUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void TreeHandler(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeView tree = sender as TreeView;
+            switch(tree.SelectedItem)
+            {
+                case "Приёмы":
+
+                    break;
+                case "Анализы":
+
+                    break;
+                case "Исследования":
+
+                    break;
+                case "Записи и направления":
+                    Frame.Content = new MainPage();
+                    break;
+            }
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
